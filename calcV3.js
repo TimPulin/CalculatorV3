219f7f296629fec92164501c924afb37e8fda3d7
TimPulin <47381676+TimPulin@users.noreply.github.com>
2020-06-07 14:26:10 +0300

    Update calcV3.js

--- calcV3.js
+++ calcV3.js
@@ -1,3 +1,41 @@
 jQuery('.boxoutput-name').click(function(){
     $('#ElementModal').modal();
 });
+
+//===========================Поведение кнопок=========================================
+
+//========================Переключение вкладок в модальном окне==
+$(document).ready(function(){
+    let Iam;
+    let Index;
+    let Title_Modal;
+    $('.tablinksCalc').click(function(){
+        Iam=$(this);
+        Index=Iam.closest('.tabCalc').find('.tablinksCalc').index(Iam);
+        Title_Modal=Iam.val();
+        AddRemove_Active();
+        ShowHide_tabel ();
+        Print_Title_Modal();
+    });
+
+    function AddRemove_Active(){
+        Iam.closest('.tabCalc').find('.tablinksCalc').each(function(index){
+            $(this).removeClass('active');
+        });
+        Iam.addClass('active');
+    };
+
+    function ShowHide_tabel(){
+        Iam.closest('.JS_Section-tables').find('.JS_Tab').each(function(index){
+            if (index==Index){
+                $(this).removeClass('hide');
+            }
+           else {$(this).addClass('hide');}
+        });
+    };
+
+    function Print_Title_Modal(){
+        Iam.closest('.JS_Section-modal').find('.header-title').html(Title_Modal);
+    };
+});
+//========================КОНЕЦ Переключение вкладок в модальном окне==
