5c43d196283fd72880a0960b53a4cb870860b41f
TimPulin <47381676+TimPulin@users.noreply.github.com>
2020-06-14 21:22:20 +0300

    Create Compiling.js

---
+++ Compiling.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+    let Iam,
+        IamModal;
+
+    $('.JS_Section-Modal .JS_Save').click(function() {
+        Iam=$(this);
+        IamModal=$(this).closest('.JS_Section-Modal');
+        DirectorCompilingConfig()
+    })
+
+    function DirectorCompilingConfig() {
+        FindActiveTab();
+
+
+    }
+
+    function FindActiveTab() {
+        MakeTheName_Modal(0);
+        IamModal.find('.tabCalc-links .tabCalc-link').each(function(index) {
+            if($(this).hasClass('active')){
+                arrActiveTabs[NameOfProperty]=index;
+                return;
+            }
+        })
+    }
+
+
+})
