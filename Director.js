0c29eb536b76a56b24724641508067846302847d
TimPulin <47381676+TimPulin@users.noreply.github.com>
2020-06-14 21:22:32 +0300

    Create Director.js

---
+++ Director.js
@@ -0,0 +1,17 @@
+//======================ГЛОБАЛЬНЫЕ служебные функции======================
+
+var ID,
+    NameOfProperty,
+    IndexT;
+let arrActiveTabs={};
+let arrButtonsClass={};
+
+
+
+
+//====создание имени====
+function MakeTheName_Modal(index) {
+    NameOfProperty='line'+ID+'_index'+index;
+    return;
+}
+//====КОНЕЦ создание имени====
