d637fa53e86f8da2d0105874d289317d0288d968
TimPulin <47381676+TimPulin@users.noreply.github.com>
2020-06-17 17:04:27 +0300

    Update MainCalculateV3.js

--- MainCalculateV3.js
+++ MainCalculateV3.js
@@ -5,8 +5,12 @@ $(document).ready(function() {
         goe,
         bonusGoe,
         coefficient,
-        compare;
-
+        compare,
+        fullscores;
+    let arrLineScores={};
+    $('.boxoutput-name').click(function() {
+        Iam=$(this);
+    })
     $('#ElementModal .JS_Save').click(function(){
         DirectorMain();
     })
@@ -18,7 +22,7 @@ $(document).ready(function() {
     })
 
     $('.JS_Section-Table .JS_X').click(function() {
-        Ich=$(this);
+        Ich=Iam=$(this);
         sectionINmain=$(this).closest('.JS_Section-El');
         GetID();
         DirectorMain();
@@ -37,16 +41,45 @@ $(document).ready(function() {
     })
 
 
-
     function DirectorMain() {
+        DirectorLine();
+        CounterMain();
+        PrinterMain();
+    }
+
+    function CounterMain() {
+        fullscores=0;
+        Iam.closest('.JS_Section-Table').find('.JS_Section-El').each(function(index) {
+            Ich=$(this).find('.boxoutput-name');
+            sectionINmain=$(this);
+            GetID();
+            MakeTheName_Modal(0);
+
+            if(arrLineScores[NameOfProperty]==undefined){
+                arrLineScores[NameOfProperty]=0;
+            }
+            fullscores=fullscores+arrLineScores[NameOfProperty];
+        })
+        return;
+    }
+
+    function PrinterMain() {
+        Ich.closest('.JS_Section-Table').find('.tableoutput-scores').text(fullscores.toFixed(2));
+    }
+//==============функции DirectorLine=====================
+    function DirectorLine() {
         MakeTheName_Modal(0);
         CheckAvailabilityInfo();
         if(CheckAvailabilityInfo()){
-        GetInfoX();
-        GetGOE();
-        ComputerMain();
-        Printer()
+            GetInfoX();
+            GetGOE();
+            CounterLIne();
+            PrinterLine();
         }
+        else {
+            arrLineScores[NameOfProperty]=0;
+        }
+        return;
     }
 
     function CheckAvailabilityInfo() {
@@ -86,21 +119,23 @@ $(document).ready(function() {
         return;
     }
 
-    function ComputerMain() {
+    function CounterLIne() {
         scores=0;
         for(let i=0; i<arrScoresInMain[NameOfProperty].length; i++){
             scores=scores+arrScoresInMain[NameOfProperty][i];
         }
         scores=scores*x+bonusGoe;
+        arrLineScores[NameOfProperty]=scores;
+
         return;
     }
 
 
-    function Printer() {
+    function PrinterLine() {
         sectionINmain.find('.lineoutput-scores').text(scores.toFixed(2));
         sectionINmain.find('.lineoutput-name').text(arrNamesInMain[NameOfProperty].join('+'));
     }
-
+//==============КОНЕЦ функции DirectorLine=====================
 
 
 
