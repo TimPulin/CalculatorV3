fb57dccaf90066609a678eca5b97eac19e039114
TimPulin <47381676+TimPulin@users.noreply.github.com>
2020-07-03 00:03:21 +0300

    Update PrintTable.js

--- PrintTable.js
+++ PrintTable.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
     let tr = '<tr class="JS_Section-El">\
                 <td class="JS_NumberOfline"></td><td class=""><div class="boxoutput-name"><output class="lineoutput-name"></output></div></td>\
-                <td><input type="button" class="JS_X JS_Button buttonGuideBasic" value="X"></td>\
-                <td><input type="button" class="JS_Goe JS_Button buttonGuideBasic" value="0"></td>\
+                <td><input type="button" class="JS_X JS_Button buttonGuideBasic" value="X" disabled></td>\
+                <td><input type="button" class="JS_Goe JS_Button buttonGuideBasic" name="goe" value="0"></td>\
                 <td><output class="lineoutput-scores">0.00</output></td>\
             </tr>';
     let i=0;
+
+    for(i=0;i<12;i++){
+        PrintTable()
+    }
+
+    function PrintTable() {
+        $(tr).appendTo('.tbody');
+        $('.JS_NumberOfline').eq(i).append(i+1)
+        return;
+    }
 
   /*setInterval(function() {
         i++
@@ -21,12 +31,4 @@ $(document).ready(function() {
 
 
 
-    for(i=0;i<12;i++){PrintTable()}
-
-
-    function PrintTable() {
-        $(tr).appendTo('.tbody');
-        $('.JS_NumberOfline').eq(i).append(i+1)
-        return;
-    }
 })
